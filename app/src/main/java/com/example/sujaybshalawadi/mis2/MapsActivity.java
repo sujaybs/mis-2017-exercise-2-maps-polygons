61ea20fedfa0853c16c10631a696e0b5f40c5544
0x0af <0x0af@ukr.net>
2017-05-03 18:28:23 +0200

    2b complete

--- app/src/main/java/com/example/sujaybshalawadi/mis2/MapsActivity.java
+++ app/src/main/java/com/example/sujaybshalawadi/mis2/MapsActivity.java
@@ -6,14 +6,17 @@ import android.content.Intent;
 import android.content.IntentSender;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.text.TextUtils;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -29,10 +32,20 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.maps.android.SphericalUtil;
+import com.mikepenz.google_material_typeface_library.GoogleMaterial;
+import com.mikepenz.iconics.IconicsDrawable;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.Map;
 
 import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
@@ -45,9 +58,15 @@ public class MapsActivity extends FragmentActivity {
     private static final String DATA_POINTS = "DATA_POINTS";
     private static final String KEY_STRINGS = "KEY_STRINGS";
     private static final String[] LOCATION_PERMISSIONS = new String[]{ACCESS_FINE_LOCATION, ACCESS_COARSE_LOCATION};
+    private static final int ACTIVE_POLYGON_NONE = -1;
+
+    // effectively final
+    private IconicsDrawable ICON_DRAW_POLYGON;
+    private IconicsDrawable ICON_STOP_POLYGON;
 
     private SharedPreferences mSharedPreferences;
-    private HashMap<String, LatLng> mTextLocationMap;
+    private HashMap<String, LatLng> mMessageLocationMap;
+    private ArrayList<LinkedHashSet<LatLng>> mPolygonLocationMap;
 
     private LocationServicesEventHandler mEventHandler;
     private MapEventHandler mMapEventHandler;
@@ -55,21 +74,39 @@ public class MapsActivity extends FragmentActivity {
     private GoogleMap mMap;
     private GoogleApiClient mGoogleApiClient;
     private EditText mEditText;
+    private Button mClearAllButton;
+    private FloatingActionButton mFloatingActionButton;
 
     private LocationRequest mLocationRequest;
     private LocationSettingsRequest mLocationSettingsRequest;
 
     private Location mCurrentLocation;
+    private boolean isDrawingPolygon = false;
+    private int activePolygonId = ACTIVE_POLYGON_NONE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
+        ICON_DRAW_POLYGON = new IconicsDrawable(this)
+                .icon(GoogleMaterial.Icon.gmd_edit_location)
+                .color(Color.WHITE)
+                .sizeDp(36);
+
+        ICON_STOP_POLYGON = new IconicsDrawable(this)
+                .icon(GoogleMaterial.Icon.gmd_done)
+                .color(Color.WHITE)
+                .sizeDp(36);
+
         mEditText = (EditText) findViewById(R.id.editText);
+        mClearAllButton = (Button) findViewById(R.id.clearAllButton);
+        mFloatingActionButton = (FloatingActionButton) findViewById(R.id.fab);
+        mFloatingActionButton.setImageDrawable(ICON_DRAW_POLYGON);
 
         mSharedPreferences = getSharedPreferences(DATA_POINTS, Context.MODE_PRIVATE);
-        mTextLocationMap = new HashMap<>();
+        mMessageLocationMap = new HashMap<>();
+        mPolygonLocationMap = new ArrayList<>();
 
         mEventHandler = new LocationServicesEventHandler();
         mMapEventHandler = new MapEventHandler();
@@ -81,9 +118,11 @@ public class MapsActivity extends FragmentActivity {
                 .build();
 
         mLocationRequest = new LocationRequest();
-        mLocationRequest.setInterval(1000);
-        mLocationRequest.setFastestInterval(1000);
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(10000);
+
+        // Preserve accu power
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_LOW_POWER);
 
         LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
         builder.addLocationRequest(mLocationRequest);
@@ -93,6 +132,52 @@ public class MapsActivity extends FragmentActivity {
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(mMapEventHandler);
+
+        mFloatingActionButton.setOnClickListener((view) -> {
+            if (!isDrawingPolygon) {
+                startPolygon();
+            } else {
+                stopPolygon();
+            }
+        });
+
+        mClearAllButton.setOnClickListener((view) -> {
+            stopPolygon();
+            mMessageLocationMap.clear();
+            mPolygonLocationMap.clear();
+            updateCurrentLocation(false);
+        });
+    }
+
+    private void startPolygon() {
+        if (mMap != null) {
+            mMap.setOnMapLongClickListener((latLng) -> {
+                LinkedHashSet<LatLng> vertexSet;
+                if (activePolygonId == ACTIVE_POLYGON_NONE) {
+                    vertexSet = new LinkedHashSet<>();
+                    mPolygonLocationMap.add(vertexSet);
+                    activePolygonId = mPolygonLocationMap.indexOf(vertexSet);
+                    vertexSet.add(latLng);
+                } else {
+                    vertexSet = mPolygonLocationMap.get(activePolygonId);
+                    vertexSet.add(latLng);
+                }
+                render();
+            });
+            mFloatingActionButton.setImageDrawable(ICON_STOP_POLYGON);
+            mEditText.setEnabled(false);
+            isDrawingPolygon = true;
+        } else {
+            stopPolygon();
+        }
+    }
+
+    private void stopPolygon() {
+        mMap.setOnMapLongClickListener(mMapEventHandler);
+        mFloatingActionButton.setImageDrawable(ICON_DRAW_POLYGON);
+        mEditText.setEnabled(true);
+        activePolygonId = ACTIVE_POLYGON_NONE;
+        isDrawingPolygon = false;
     }
 
     private void updateCurrentLocation(boolean doMoveCamera) {
@@ -101,22 +186,74 @@ public class MapsActivity extends FragmentActivity {
         }
 
         LatLng myLatLng = new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
-        mTextLocationMap.put("I am here!", myLatLng);
-        renderMarkers();
+        mMessageLocationMap.put(getString(R.string.i_am_here), myLatLng);
+        render();
 
         if (doMoveCamera) {
-            mMap.animateCamera(CameraUpdateFactory.newLatLng(myLatLng));
+            mMap.animateCamera(CameraUpdateFactory.zoomTo(16.0f), new GoogleMap.CancelableCallback() {
+                @Override
+                public void onFinish() {
+                    mMap.animateCamera(CameraUpdateFactory.newLatLng(myLatLng));
+                }
+
+                @Override
+                public void onCancel() {
+                    // do nothing
+                }
+            });
+        }
+    }
+
+    private void render() {
+        if (mMap != null) {
+            mMap.clear();
+            renderMarkers();
+            renderPolygons();
         }
     }
 
     private void renderMarkers() {
-        if (mMap != null) {
-            mMap.clear();
-            for (Map.Entry<String, LatLng> marker : mTextLocationMap.entrySet()) {
-                mMap.addMarker(new MarkerOptions().position(marker.getValue()).title(marker.getKey()));
-            }
+        for (Map.Entry<String, LatLng> marker : mMessageLocationMap.entrySet()) {
+            mMap.addMarker(new MarkerOptions().position(marker.getValue()).title(marker.getKey()));
         }
     }
+
+    private void renderPolygons() {
+        for (LinkedHashSet<LatLng> vertex : mPolygonLocationMap) {
+            if (vertex.isEmpty())
+                continue;
+
+            LatLngBounds.Builder centroidBuilder = new LatLngBounds.Builder();
+
+            //noinspection ForLoopReplaceableByForEach
+            for (Iterator<LatLng> iterator = vertex.iterator(); iterator.hasNext(); ) {
+                LatLng marker = iterator.next();
+                mMap.addMarker(new MarkerOptions().position(marker).alpha(0.5F));
+                centroidBuilder.include(marker);
+            }
+
+            double area = SphericalUtil.computeArea(new ArrayList<>(vertex));
+
+            BigDecimal bigDecimalArea = new BigDecimal(area > 1000000d ? area / 1000000d : area);
+            bigDecimalArea = bigDecimalArea.round(new MathContext(6));
+
+            if (vertex.size() > 2)
+                mMap.addMarker(new MarkerOptions()
+                        .position(centroidBuilder.build().getCenter())
+                        .alpha(1F)
+                        .title(String.format("%s%s",
+                                String.valueOf(bigDecimalArea.doubleValue()),
+                                area > 1000000d ? getString(R.string.sq_km) : getString(R.string.sq_m))));
+
+            mMap.addPolygon(new PolygonOptions()
+                    .geodesic(false)
+                    .addAll(vertex)
+                    .strokeWidth(6.0F)
+                    .strokeColor(getResources().getColor(R.color.colorAccent))
+                    .fillColor(getResources().getColor(R.color.colorPrimaryTransparent)));
+        }
+    }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -173,18 +310,18 @@ public class MapsActivity extends FragmentActivity {
             tryRequestLocationUpdates();
         }
         loadMarkers();
-        renderMarkers();
+        render();
     }
 
     private void loadMarkers() {
-        if (mTextLocationMap != null && mSharedPreferences != null) {
+        if (mMessageLocationMap != null && mSharedPreferences != null) {
             HashSet<String> messages = (HashSet<String>) mSharedPreferences.getStringSet(KEY_STRINGS, new HashSet<>());
             for (String message : messages) {
                 float lat = mSharedPreferences.getFloat(message + "_lat", -1f);
                 float lon = mSharedPreferences.getFloat(message + "_lon", -1f);
                 if (lat != -1f && lon != -1f) {
                     LatLng latLng = new LatLng(lat, lon);
-                    mTextLocationMap.put(message, latLng);
+                    mMessageLocationMap.put(message, latLng);
                 }
             }
         }
@@ -200,10 +337,10 @@ public class MapsActivity extends FragmentActivity {
     }
 
     private void saveMarkers() {
-        if (mTextLocationMap != null && !mTextLocationMap.isEmpty()) {
+        if (mMessageLocationMap != null && !mMessageLocationMap.isEmpty()) {
             SharedPreferences.Editor editor = mSharedPreferences.edit();
-            editor.putStringSet(KEY_STRINGS, mTextLocationMap.keySet());
-            for (Map.Entry<String, LatLng> marker : mTextLocationMap.entrySet()) {
+            editor.putStringSet(KEY_STRINGS, mMessageLocationMap.keySet());
+            for (Map.Entry<String, LatLng> marker : mMessageLocationMap.entrySet()) {
                 editor.putFloat(marker.getKey() + "_lat", (float) marker.getValue().latitude);
                 editor.putFloat(marker.getKey() + "_lon", (float) marker.getValue().longitude);
             }
@@ -223,8 +360,8 @@ public class MapsActivity extends FragmentActivity {
         public void onMapLongClick(LatLng latLng) {
             if (mEditText != null && !TextUtils.isEmpty(mEditText.getText())) {
                 String message = mEditText.getText().toString();
-                mTextLocationMap.put(message, latLng);
-                renderMarkers();
+                mMessageLocationMap.put(message, latLng);
+                render();
             } else {
                 Snackbar.make(findViewById(R.id.map), R.string.mess_not_set, Snackbar.LENGTH_SHORT).show();
             }
@@ -241,20 +378,14 @@ public class MapsActivity extends FragmentActivity {
 
         @Override
         public void onLocationChanged(Location location) {
+            boolean doMoveCamera = mCurrentLocation == null;
             mCurrentLocation = location;
-            updateCurrentLocation(false);
+            updateCurrentLocation(doMoveCamera);
         }
 
         @Override
         public void onConnected(Bundle connectionHint) {
-            if (mCurrentLocation == null) {
-                if (ActivityCompat.checkSelfPermission(MapsActivity.this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                        && ActivityCompat.checkSelfPermission(MapsActivity.this, ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    return;
-                }
-                mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-                updateCurrentLocation(true);
-            }
+            // do nothing
         }
 
         @Override
